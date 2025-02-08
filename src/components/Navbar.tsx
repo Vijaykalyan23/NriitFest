090724d47cb2185f015c61218ef23b12b01389eb
Vijay Kalyan <vijaykalyan4433@gmail.com>
2025-02-08 10:53:59 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,9 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <img src={Logo} alt="Sunrise 2025" className="h-28 w-auto object-contain " />
+            <a href="home">
+            <img src={Logo} alt="Sunrise 2025" className="h-28 w-auto object-contain "  />
+            </a>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-8">
